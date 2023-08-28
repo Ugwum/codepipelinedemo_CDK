1e1cd92230e26a2d3903d51d08a59e75cb78598f
ugwum <obieziefule@gmail.com>
2023-08-28 04:25:16 +0100

    Update pipeline

--- lib/codepipelinedemo_cdk-stack.ts
+++ lib/codepipelinedemo_cdk-stack.ts
@@ -12,7 +12,7 @@ export class CodepipelinedemoCdkStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
+        input: CodePipelineSource.gitHub('ugwum/codepipelinedemo_CDK', 'main'), //Remember to change 
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
